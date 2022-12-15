8227fa1c21ce928ce615c33208e0d82765014319
honorable85 <honorable85@gmail.com>
2022-12-15 17:43:14 +0100

    Fixed dependency bug - Flesco from Clicalmani

--- app/http/controllers/TestController.php
+++ app/http/controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Clicalmani\Flesco\Http\Controllers\RequestController;
 use App\Http\Requests\TestRequest;
 use App\Models\TestModel;
+use Clicalmani\XPower\XDT;
 
 class TestController extends RequestController
 {
@@ -17,10 +18,9 @@ class TestController extends RequestController
 
     function hash($request)
     {
+        $xdt = new XDT;
         $model = new TestModel;
         echo '<pre>'; print_r($model->where('id_compte = 1')->get('privilege')); echo '</pre>';
         echo 'Hash: ' . $request->verifyParameters(); 
-        echo '<br>';
-        echo 'Mail from: ' . env('MAIL_FROM_NAME');
     }
 }
